9617bdb95bfb73ea802e8b6dcbb3574288a76ec6
wuyuan0123 <505609087@qq.com>
2016-06-03 10:02:26 +0800

    day3

---
+++ day3_function.py
@@ -0,0 +1,30 @@
+import requests
+def spider(url):
+    r = requests.get(url)
+    # print r.text
+    s = "<img"
+    count = 0
+    start_pos = 0
+    flag = 0
+    img_list = []
+
+    for i in range(len(r.text)):
+        target = r.text[i:i + len(s)]
+        if target == s:
+            # print target
+            for j in range(i, len(r.text)):
+                if r.text[j] == '"':
+                    if flag == 0:
+                        start_pos = j + 1
+                        flag = 1
+                    else:
+                        result = r.text[start_pos: j]
+                        flag = 0
+                        break
+            if result.endswith("jpg"):
+                img_list.append(result)
+
+    for i in img_list:
+        print i
+
+print spider('https://movie.douban.com/chart')
