8687bf5183de729b573f500dafc00d95f93101e8
wuyuan0123 <505609087@qq.com>
2016-06-21 16:45:34 +0800

    print time

--- regular_expression.py
+++ regular_expression.py
@@ -1,10 +1,16 @@
 import requests, time, re
-t1 = time.time()
 def spider(url):
     soure = requests.get(url)
     pattern = re.compile(r"(https://.*?[\.]jpg)")
     img_list = re.findall(pattern, soure.text)
     return img_list
+t1 = time.time()
 imgs = spider('https://movie.douban.com/chart')
+t2 = time.time()
 for img in imgs:
     print img
+t3 = time.time()
+print("time = %.6f sec" % (t2-t1))
+print("time = %.6f sec" % (t3-t1))  #result : t3-t1 = t2 -t1 time = 0.671000 sec
+
+
