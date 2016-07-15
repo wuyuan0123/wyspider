84a633aa734ae5af409b807e1b85091fd7a3c6d0
wuyuan0123 <505609087@qq.com>
2016-07-15 16:53:49 +0800

    bs4

--- day4.py
+++ day4.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 import time, requests
 def spider(url):
     r = requests.get(url)
-    soup = BeautifulSoup(r, "html.parser")
-    for link in soup.find_all('img'):
-        print(link.get('href'))
+    #print r.text
+    soup = BeautifulSoup(r.text, "html.parser")
+    for link in soup.find_all('https'):
+        print(link.get('src'))
 t1 = time.time()
 spider('https://movie.douban.com/chart')
-t2 = time.time
+t2 = time.time()
 print("time = %.6f sec" % (t2-t1))
