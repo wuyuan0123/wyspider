91fa384e45dbd428b3a87cf7e5f05710c8b8b91d
wuyuan0123 <505609087@qq.com>
2016-11-03 17:57:59 +0800

    add

--- spider_github.py
+++ spider_github.py
@@ -1,17 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
-username = "chenyoufu"
-url = "https://github.com/{0}?tab=followers".format(username)
-r = requests.get(url)
-soup = BeautifulSoup(r.text)
-tmps = soup.find_all("span", attrs={"class": "f4 link-gray-dark"})
-print tmps
-print tmps[0].text
-user = []
-for tmp in tmps:
-    user.append(tmp.text)
-print user
-print len(user)
+users_list= []
+
+def get_user(username):
+    global users_list
+    users = []
+    url = "https://github.com/{0}?tab=followers".format(username)
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    tmps = soup.find_all("span", attrs={"class": "link-gray pl-1"})
+    for tmp in tmps:
+        users.append(tmp.text)
+    for tmp in tmps:
+        if tmp.text not in users_list:
+            users_list.append(tmp.text)
+    return users,len(users)
+
+
+
+print get_user("chenyoufu")
+
+for user in users_list:
+    print user
+    get_user(user)
+    print users_list
+    print len(users_list)
+
+
+
+
+
+
+
+
+
 
 
 
